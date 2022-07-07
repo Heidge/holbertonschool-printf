c4e101e8080649be900a6d0aa511dc9f2912c2bb
Hedgebzh <axel.gore35@gmail.com>
2022-07-07 14:19:50 +0200

    create functions printchar and putchar

---
+++ _put_s.c
@@ -0,0 +1,14 @@
+#include "main.h" // // include all necessaries libraries
+
+/**
+ * _puts - print a string
+ *
+ * @str: string random
+ */
+int _puts(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++) // incrementation of c for run all the string
+	return(str[c]); // return string when c have run all of it
+}
